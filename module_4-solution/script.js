c7cd391c9255016f7f34f82360541a42fed690e6
khushal312 <98796842+khushal312@users.noreply.github.com>
2022-04-12 01:20:55 -0400

    Update script.js

--- module_4-solution/script.js
+++ module_4-solution/script.js
@@ -1,15 +1,14 @@
 (function () {
-
-    var names = ["Khushal", "Jessi", "Vadwala", "Jackson", "Jena", "Paul", "Lily", "Patrick", "Leo", "Jimmy"];
-
+  
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
     for (var i = 0; i < names.length; i++) {
   
        var firstLetter = names[i].charAt(0).toLowerCase();
   
       if (firstLetter === 'j') {
          byeSpeaker.speak(names[i]);
-      }
-      else {
+      } else {
         helloSpeaker.speak(names[i]);
       }
     }

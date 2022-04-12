8c9b3915a3098f44604b6c6f5769357b44ae1ffb
khushal312 <98796842+khushal312@users.noreply.github.com>
2022-04-12 01:17:25 -0400

    Add files via upload

---
+++ module_4-solution/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+    var names = ["Khushal", "Jessi", "Vadwala", "Jackson", "Jena", "Paul", "Lily", "Patrick", "Leo", "Jimmy"];
+
+    for (var i = 0; i < names.length; i++) {
+  
+       var firstLetter = names[i].charAt(0).toLowerCase();
+  
+      if (firstLetter === 'j') {
+         byeSpeaker.speak(names[i]);
+      }
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+  })()
